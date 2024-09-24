d2fcf1d86461708385f8f0ff9a5384096b79ed35
Edouard Leroy <leroyedouard@protonmail.com>
2024-09-24 13:16:17 +0200

    Migration Spring boot V3

--- src/main/java/com/mycompany/invoise/product/ProductApplication.java
+++ src/main/java/com/mycompany/invoise/product/ProductApplication.java
@@ -1,6 +1,7 @@
 package com.mycompany.invoise.product;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -15,10 +16,10 @@ public class ProductApplication {
 	}
 
 	@Bean
-	public Hibernate5Module datatypeHibernateModule() {
-		Hibernate5Module module= new Hibernate5Module();
-		module.disable(Hibernate5Module.Feature.USE_TRANSIENT_ANNOTATION);
-        module.enable(Hibernate5Module.Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS);
+	public Hibernate6Module datatypeHibernateModule() {
+		Hibernate6Module module= new Hibernate6Module();
+		module.disable(Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION);
+        module.enable(Hibernate6Module.Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS);
 		return module;
 	}
 }
